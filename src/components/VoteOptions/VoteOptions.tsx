63821d3d19c8ef39e481b876c3e32f86846f6f0c
Taisiia Shloma <shloma20082004@icloud.com>
2026-09-06 18:43:33 +0200

    update project structure

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,4 +1,4 @@
-import type { VoteType } from "../types/votes"
+import type { VoteType } from "../../types/votes"
 import styles from "./VoteOptions.module.css"
 
 interface VoteOptionsProps {
--- src/components/VoteStats/VoteStats.tsx
+++ src/components/VoteStats/VoteStats.tsx
@@ -1,4 +1,4 @@
-import type { Votes } from "../types/votes"
+import type { Votes } from "../../types/votes"
 import styles from "./VoteStats.module.css"
 
 interface VoteStatProps {
